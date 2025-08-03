11a12168cc37a0d713380b3249a3550ee96ffbdf
Joseph <panimayajoseph94@gmail.com>
2025-08-03 05:58:01 +0100

    updated deploy address -Sepolia

--- frontend/iot-frontend-marketpalce/src/constants.js
+++ frontend/iot-frontend-marketpalce/src/constants.js
@@ -6,12 +6,11 @@ import DeviceRegistryArtifact from './abis/DeviceRegistry.json';
 import DataPublisherArtifact from './abis/DataPublisher.json';
 import MarketplaceArtifact from './abis/Marketplace.json';
 
-
-export const DEVICE_REGISTRY_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const DEVICE_REGISTRY_ADDRESS = "0x1B5ca695e3df1498a7d77B75c051125F403fdfB6";
 export const DEVICE_REGISTRY_ABI = DeviceRegistryArtifact.abi;
 
-export const DATA_PUBLISHER_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+export const DATA_PUBLISHER_ADDRESS = "0x1C957b7c1745A3D9ad429EFF34CFD7d4949Ac231";
 export const DATA_PUBLISHER_ABI = DataPublisherArtifact.abi;
 
-export const MARKETPLACE_ADDRESS = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+export const MARKETPLACE_ADDRESS = "0xD2646a6eDF70b9276790d5a2EE11fECaFD56675B";
 export const MARKETPLACE_ABI = MarketplaceArtifact.abi;
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require('dotenv').config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.API_KEY}`,
+      accounts: [`0x${process.env.WALLET_PRIVATE_KEY}`],
+    },
+    // add other networks if needed
+  }
 };
+
